5a355778e7f55bea97efc1a55015ae4edc772068
Cédric Béthencourt <522447+cedricus@users.noreply.github.com>
2020-11-21 18:03:55 -0500

    Fix test.php and add travis file for later testing

--- test.php
+++ test.php
@@ -1,37 +1,35 @@
 <?php
-
 include 'vendor/autoload.php';
 require_once('core/pos_info.php');
 
 use Gettext\Translations;
 use satellitewp\po;
 
-
 $pi = new \satellitewp\po\Pos_Info( 'fr-ca', 'fr' );
 $pi->set_url( 'https://wordpress.org/plugins/wordpress-seo/' );
 
 $vars = $pi->get_internal();
 
-#var_dump( $vars );
-/*
 echo 'name: ' . $pi->get_project_name();
 echo "\n";
 echo 'type: ' . $pi->get_project_type();
 echo "\n";
-echo "\n";*/
+echo "\n";
 echo $pi->get_base_download_url();
 echo "\n";
 echo $pi->get_copy_download_url();
+echo "\n";
 
-
-exit;
-
-$param = $argv[1];
+if(true === empty($argv[1])) {
+    $param = '100';
+} else {
+    $param = $argv[1];
+}
 
 $base = Translations::fromPoFile('tests/locale/fr-ca/'. $param .'-fr-ca.po');
 $copy = Translations::fromPoFile('tests/locale/fr/'. $param .'-fr.po');
 
-//var_dump( count( $base ) ); exit;
+var_dump( count( $base ) );
 
 foreach( $base as $tr) {
     
@@ -45,8 +43,6 @@ foreach( $base as $tr) {
                 $tr->setPluralTranslations( $copy_tr->getPluralTranslations() );
             }
         }
-
-        //$tr->addFlag('Maxime');
     }
 }
 
@@ -57,7 +53,6 @@ $exp = Translations::fromPoFile('tests/locale/expected/'. $param .'-expected-fr-
 
 if ($res->toPoString() != $exp->toPoString()) {
     echo "different";
-}
-else {
+} else {
     echo "ok";
 }
