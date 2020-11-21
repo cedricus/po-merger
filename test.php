1ef18b92157e8ada60bd2b37b54520e23f8f99dc
Cédric Béthencourt <522447+cedricus@users.noreply.github.com>
2020-11-21 18:49:19 -0500

    Testing all files but not working I do not know why... I saw some PHPUNIT tests in the folder. Need to try with Travis

--- test.php
+++ test.php
@@ -5,54 +5,49 @@ require_once('core/pos_info.php');
 use Gettext\Translations;
 use satellitewp\po;
 
-$pi = new \satellitewp\po\Pos_Info( 'fr-ca', 'fr' );
+$baseLocale = 'fr-ca';
+$copyLocale = 'fr';
+$tests = array('100', '101', '102', '103');
+$error = 0;
+
+$pi = new \satellitewp\po\Pos_Info($baseLocale, $copyLocale);
 $pi->set_url( 'https://wordpress.org/plugins/wordpress-seo/' );
 
 $vars = $pi->get_internal();
 
-echo 'name: ' . $pi->get_project_name();
-echo "\n";
-echo 'type: ' . $pi->get_project_type();
-echo "\n";
-echo "\n";
-echo $pi->get_base_download_url();
-echo "\n";
-echo $pi->get_copy_download_url();
-echo "\n";
+echo 'name: ' . $pi->get_project_name()."\n";
+echo 'type: ' . $pi->get_project_type()."\n";
 
-if(true === empty($argv[1])) {
-    $param = '100';
-} else {
-    $param = $argv[1];
-}
+foreach ($tests as $test) {
+    $base = Translations::fromPoFile('tests/locale/'.$baseLocale.'/'. $test .'-'.$baseLocale.'.po');
+    $copy = Translations::fromPoFile('tests/locale/'.$copyLocale.'/'. $test .'-'.$copyLocale.'.po');
 
-$base = Translations::fromPoFile('tests/locale/fr-ca/'. $param .'-fr-ca.po');
-$copy = Translations::fromPoFile('tests/locale/fr/'. $param .'-fr.po');
+    foreach( $base as $tr) {
+        
+        if ( ! $tr->hasTranslation() ) {
+            $copy_tr = $copy->find( $tr->getContext(), $tr->getOriginal() );
 
-var_dump( count( $base ) );
+            if ($copy_tr !== false) {
+                $tr->setTranslation( $copy_tr->getTranslation() );
 
-foreach( $base as $tr) {
-    
-    if ( ! $tr->hasTranslation() ) {
-        $copy_tr = $copy->find( $tr->getContext(), $tr->getOriginal() );
-
-        if ($copy_tr !== false) {
-            $tr->setTranslation( $copy_tr->getTranslation() );
-
-            if ($tr->hasPluralTranslations()) {
-                $tr->setPluralTranslations( $copy_tr->getPluralTranslations() );
+                if ($tr->hasPluralTranslations()) {
+                    $tr->setPluralTranslations( $copy_tr->getPluralTranslations() );
+                }
             }
         }
     }
+
+    $base->toPoFile('result.po');
+
+    $res = Translations::fromPoFile('result.po');
+    $exp = Translations::fromPoFile('tests/locale/expected/'. $test .'-expected-'.$baseLocale.'.po');
+
+    if ($res->toPoString() != $exp->toPoString()) {
+        echo $test.' FAILED'."\n";
+        $error = 1;
+    } else {
+        echo $test.' SUCCEED'."\n";
+    }
 }
 
-$base->toPoFile('result.po');
-
-$res = Translations::fromPoFile('result.po');
-$exp = Translations::fromPoFile('tests/locale/expected/'. $param .'-expected-fr-ca.po');
-
-if ($res->toPoString() != $exp->toPoString()) {
-    echo "different";
-} else {
-    echo "ok";
-}
+exit($error);
